f1045f348484e153aca9d35b5da5e61b7d2fa814
Chad Heisler <chadheisler5@gmail.com>
2020-10-08 12:02:39 -0700

    updated route to questions

--- app.py
+++ app.py
@@ -12,9 +12,22 @@ debug = DebugToolbarExtension(app)
 responses = list()
 
 
-
 @app.route("/")
 def show_home():
     """Home Page"""
 
     return render_template("/survey_start.html")
+
+@app.route("/begin", methods=["POST"])
+def survey_redict():
+
+    return redirect("/question/0")
+
+
+@app.route("/question/<num>")
+def handle_questions(num):
+    """Handling the questions"""
+
+    question = survey.questions[int(num)]
+
+    return render_template("/question.html", question=question)
